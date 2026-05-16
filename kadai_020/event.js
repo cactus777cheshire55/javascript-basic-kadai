e11e38be5dd83cbae7bdfbb5e15dd09ea3071f60
cactus777cheshire55 <126995348+cactus777cheshire55@users.noreply.github.com>
2026-05-16 18:55:05 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,5 +1,5 @@
 const pushBtn = document.getElementById('btn');
 
 pushBtn.addEventListener('click', () => {
-  document.getElementById('text').textContent = 'ボタンをクリックしました';
+  text.textContent = 'ボタンをクリックしました';
 });
